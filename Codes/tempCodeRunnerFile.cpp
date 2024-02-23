94f568c1c842803f8dd00987d3d7c19d5e4dc126
goel-aadhaar <goel.aadhaar@outlook.com>
2024-02-23 23:09:08 +0530

    added 15 codes

---
+++ Codes/tempCodeRunnerFile.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int sum1=0;
+    int sum2=0;
+    int sum3=0;
+    int arr[n][3];
+    for(int i=0;i<=n-1;i++){
+        for(int j=0;j<=2;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i=0;i<=n-1;i++){
+        for(int j=0;j<=2;j++){
+            if(j==0){
+                sum1=sum1+arr[i][0];
+            }
+            if(j==1){
+                sum2=sum2+arr[i][1];
+            }
+            if(j==2){
+                sum3=sum3+arr[i][2];
+            }
+        }
+    }
+    if(sum==0){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}
